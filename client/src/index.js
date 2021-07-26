2925853708f79863b42c02ad17315813a376561f
Ashish Kumar Pandey <ashish9145826@gmail.com>
2021-07-26 14:27:07 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
